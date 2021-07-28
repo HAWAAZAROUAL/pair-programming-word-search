081098b92466f8942ea2f0c41fd647559f538372
Hawaa <hawaazaroual@gmail.com>
2021-07-28 17:56:26 -0600

    completed PP with Declan and Vincent

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,51 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it('Should return true if the word is present vertically', function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'M', 'U', 'A', 'L'],
+      ['S', 'X', 'P', 'T', 'I', 'F', 'Q', 'R'],
+      ['Y', 'F', 'C', 'F', 'C', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'H', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'A', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'E', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'L', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'MICHAEL')
+
+    assert.isTrue(result);
+  });
+  it('Should return false if given an empty array', function() {
+    const result = wordSearch([], 'ANY WORD');
+    assert.isFalse(result);
+  });
+  it('Should return true if the word is present backwards horizontally', function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'H', 'U', 'A', 'L'],
+      ['S', 'X', 'P', 'T', 'Q', 'F', 'Q', 'R'],
+      ['Y', 'F', 'C', 'F', 'R', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'T', 'V', 'R', 'G'],
+      ['W', 'H', 'E', 'S', 'A', 'R', 'H', 'P'],
+      ['B', 'F', 'R', 'E', 'G', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'K', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'P', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'F', 'U', 'A', 'L'],
+    ], 'PHRASE')
+    assert.isTrue(result);
+  });
+  it('Should return true if word is present backwards vertically', () => {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'H', 'U', 'A', 'L'],
+      ['S', 'X', 'P', 'T', 'Y', 'F', 'Q', 'R'],
+      ['Y', 'F', 'C', 'F', 'T', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'S', 'V', 'R', 'G'],
+      ['W', 'H', 'E', 'S', 'O', 'R', 'H', 'P'],
+      ['B', 'F', 'R', 'E', 'R', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'F', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'P', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'F', 'U', 'A', 'L'],
+    ], 'FROSTY')
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,62 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    const horizontalArray = letters.map(ls => ls);
+    // console.log(horizontalArray)
+    if (letters.length === 0) {
+        return false;
+    }
+    const transposed = transpose(letters);
+    const verticalJoin = transposed.map(ls => ls.join(''));
+    const verticalArray = transposed
+    console.log(verticalArray)
+    
+    for (nested of horizontalArray) {
+        const reversed = nested.reverse().join('');
+        if (reversed.includes(word)) return true;
+    }
+    for (nested of verticalArray) {
+        const reversed = nested.reverse().join('');
+        if (reversed.includes(word)) return true;
+    }
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    for (l of horizontalJoin) {    
         if (l.includes(word)) return true
     }
+    return false;
 }
 
+
+const transpose = function (matrix) {
+    // Put your solution here
+    let newMatrix = [];
+    if (matrix.length === 1) {
+      for (let i = 0; i < matrix[0].length; i++) {
+        newMatrix.push([matrix[0][i]]);
+      }
+      return newMatrix;
+    }
+    for (let i = 0; i < matrix[0].length; i++) {
+      newMatrix.push([]);
+      for (let j = 0; j < matrix.length; j++) {
+        newMatrix[i].push(matrix[j][i]);
+      }
+    }
+    return newMatrix;
+  };
+
 module.exports = wordSearch
+
+
+// const result = wordSearch([
+//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//   ], 'SEINFELD')
